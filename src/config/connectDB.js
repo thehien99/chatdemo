68ab4c28859209ca5b391cdd67b7f7818dc4dd31
thehien99 <thehien9299@gmail.com>
2024-03-27 20:05:53 +0700

    6

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,9 +3,14 @@ require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
 });
 
 let connectDb = async () => {
